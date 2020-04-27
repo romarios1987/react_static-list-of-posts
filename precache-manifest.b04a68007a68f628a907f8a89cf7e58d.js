f050936a3615b22e84e0ca3255eef5a98070a0aa
romarios1987 <romios047@gmail.com>
2020-04-27 10:38:32 +0300

    Updates

---
+++ precache-manifest.b04a68007a68f628a907f8a89cf7e58d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bded4e1255843dbb4a623d8428a2a776",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d630424bfb2e68fd0e06",
+    "url": "./static/css/main.ae1300f5.chunk.css"
+  },
+  {
+    "revision": "6e14b9c9c5b2310e2143",
+    "url": "./static/js/2.52b7182f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.52b7182f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d630424bfb2e68fd0e06",
+    "url": "./static/js/main.48b311c2.chunk.js"
+  },
+  {
+    "revision": "3708a39cbc10f7e3c3bc",
+    "url": "./static/js/runtime-main.4f79b291.js"
+  }
+]);
